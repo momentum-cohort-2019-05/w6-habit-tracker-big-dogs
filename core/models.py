bf252cd1c7d32877a7d9f53b73e27fb16dd78f36
Clinton Dreisbach <clinton@dreisbach.us>
2019-07-02 16:53:25 -0400

    Add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Habit(models.Model):
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    daily_goal = models.PositiveIntegerField()
+    started_on = models.DateField(auto_now_add=True)
+
+
+class DailyRecord(models.Model):
+    habit = models.ForeignKey(to=Habit, on_delete=models.CASCADE)
+    date = models.DateField()
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['habit', 'date'], name="habit_date")
+        ]
