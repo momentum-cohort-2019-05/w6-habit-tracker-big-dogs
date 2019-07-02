991b4bef1a99a621bd7bef3e850f12061ca29d5c
Clinton Dreisbach <clinton@dreisbach.us>
2019-07-02 17:03:05 -0400

    Update models

--- core/models.py
+++ core/models.py
@@ -5,14 +5,30 @@ User = get_user_model()
 
 
 class Habit(models.Model):
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(to=User,
+                              on_delete=models.CASCADE,
+                              related_name="habits")
+    verb = models.CharField(max_length=255)
+    noun = models.CharField(max_length=255)
     daily_goal = models.PositiveIntegerField()
     started_on = models.DateField(auto_now_add=True)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['verb', 'noun'], name="verb_noun")
+        ]
+
+    def __str__(self):
+        if not (self.verb and self.noun and self.daily_goal):
+            return super().__str__()
+
+        return f"{self.verb} {self.daily_goal} {self.noun}"
+
 
 class DailyRecord(models.Model):
-    habit = models.ForeignKey(to=Habit, on_delete=models.CASCADE)
+    habit = models.ForeignKey(to=Habit,
+                              on_delete=models.CASCADE,
+                              related_name="daily_records")
     date = models.DateField()
     quantity = models.PositiveIntegerField()
 
--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.db.models import Max
 # Create your views here.
+
+
+@login_required
+def dashboard(request):
+    habits = request.user.habits.annotate(
+        last_updated=Max('daily_records__date'))
+    return render(request, "core/dashboard.html", {"habits": habits})
